85a39f334646044699fbc45045f5b1af78c0e520
tasawernawaz <tasawer.nawaz@arbisoft.com>
2015-01-23 15:55:26 +0500

    code improved, grid base layout used

--- todo_tasks/tasks/urls.py
+++ todo_tasks/tasks/urls.py
@@ -1,12 +1,13 @@
 from django.conf.urls import url, patterns
-from tasks.views import  user_login, my_tasks, delete, send_email, user_logout, user_signup
+from tasks.views import user_login, my_tasks, delete, send_email, user_logout, user_signup
+
 urlpatterns = patterns("",
-                        url(r"^login/$", user_login, name="login"),
-                        url(r"^logout/$", user_logout, name="logout"),
-                        url(r"^signup/$", user_signup, name="signup"),
-                        url(r"^my_tasks/$", my_tasks, name="my_tasks"),
-                        url(r"^delete/$", delete, name="delete"),
-                        url(r"^email/$", send_email, name="email")
+                       url(r"^login/$", user_login, name="login"),
+                       url(r"^logout/$", user_logout, name="logout"),
+                       url(r"^signup/$", user_signup, name="signup"),
+                       url(r"^my_tasks/$", my_tasks, name="my_tasks"),
+                       url(r"^delete/$", delete, name="delete"),
+                       url(r"^email/$", send_email, name="email")
 
 )
 
